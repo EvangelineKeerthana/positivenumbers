a4108655656ccc69bcd478027697139fc0d35820
EvangelineKeerthana <53506521+EvangelineKeerthana@users.noreply.github.com>
2021-08-24 17:31:29 +0530

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,13 @@
+list1 = [12, -7, 5, 64, -14]
+
+print("Positive nos in list1")
+for n in list1:
+    if  n>0:
+        print(n)
+        
+list2 = [12, 14, -95, 3]
+
+print("Positive nos in list2")
+for n in list2:
+    if  n>0:
+        print(n)
